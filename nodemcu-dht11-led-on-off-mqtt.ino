29e5679214e9c1802d818e24dcb3a475505c579c
olarik surinta <35139872+mrolarik@users.noreply.github.com>
2019-01-20 12:08:17 +0700

    Update nodemcu-dht11-led-on-off-mqtt.ino

--- nodemcu-dht11-led-on-off-mqtt.ino
+++ nodemcu-dht11-led-on-off-mqtt.ino
@@ -8,19 +8,18 @@ DHT dht(DHTPin, DHTTYPE);
 
 //const int lamp = 12;     //D6 = GPIO 12
 #define lamp D6
-//#define LED D6
 #define lampOnline D7
 
 
 // Update these with values suitable for your network.
-const char* ssid = "Mrolarik";
-const char* password = "olarik_home";
-const char* mqtt_server = "203.158.199.14"; //IP Address or Domain ของ MQTT Server
-const char mqtt_username[50] = "admin"; //Username ของ MQTT Server
-const char mqtt_password[50] = "1234"; //Password ของ MQTT Server
-const int mqtt_port = 11883;
+const char* ssid = "xxxx";
+const char* password = "xxxx";
+const char* mqtt_server = "111.111.111.111"; //IP Address or Domain ของ MQTT Server
+const char mqtt_username[50] = "xxxx"; //Username ของ MQTT Server
+const char mqtt_password[50] = "xxxx"; //Password ของ MQTT Server
+const int mqtt_port = 1883;
 
-const char node_mcu_id[50] = "Faculty-of-Informatics-ITxxx"; // node_id
+const char node_mcu_id[50] = "xxx"; // node_id
 String ipaddress;
 
 WiFiClient espClient;
@@ -29,7 +28,6 @@ PubSubClient client(espClient);
 // Timers auxiliar variables
 long now = millis();
 long lastMeasure = 0;
-
 
 void setup_wifi() {
    delay(10);
@@ -50,7 +48,6 @@ void setup_wifi() {
    Serial.println(WiFi.localIP());
 
    digitalWrite(lampOnline, HIGH);
-
    ipaddress = WiFi.localIP().toString();
 }
 
@@ -97,11 +94,9 @@ void reconnect() {
 }
 
 void setup() {
-  //pinMode(LED, OUTPUT);
   pinMode(lamp, OUTPUT);
   pinMode(lampOnline, OUTPUT);
   dht.begin();
-  Serial.println("humidity and temperature \n");
   delay(1000);
   
   Serial.begin(115200);
@@ -111,9 +106,8 @@ void setup() {
 }
 
 void loop() {
-   
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
     
   if(!client.connected()) {
     reconnect();
@@ -123,47 +117,21 @@ void loop() {
   }
     
   now = millis();
-
   if(now-lastMeasure > 5000) {
     lastMeasure = now;
-    //float h = dht.readHumidity();
-    //float t = dht.readTemperature();
-    //float f = dht.readTemperature(true);
 
     if(!isnan(h) || !isnan(t)) {
-      //Serial.println("Failed to read from DHT sensor!");
-      //return;
      Serial.print("Humidity: ");
      Serial.print(h);
      Serial.print(" %\t Temperature: ");
      Serial.print(t);
      Serial.println("C ");
-    
-
-    // Computes temperature values in Celsius
-    float hic = dht.computeHeatIndex(t, h, false);
-    static char temperatureTemp[7];
-    dtostrf(hic, 6, 2, temperatureTemp);
-
-    static char humidityTemp[7];
-    dtostrf(h, 6, 2, humidityTemp);
 
     // Publishes temperature and humidity values
     client.publish("room/temperature", String(t).c_str());
     client.publish("room/humidity", String(h).c_str());
     client.publish("room/nodeid", node_mcu_id);
     client.publish("room/nodeipaddress", String(ipaddress).c_str());
-    
     }
-    //Serial.println("Humidity: ");
-    //Serial.print(h);
-    //Serial.print(" %\t Temperature: ");
-    //Serial.print(t);
-    //Serial.print(" *C ");
-    //Serial.print(f);
-    //Serial.print(" *F\t Heat index: ");
-    //Serial.print(hic);
-    //Serial.println(" *C ");
-   
   }
 }
